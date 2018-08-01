65657f7c4c4da01fbaa3fbe48fc1b81eaa4aa664
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-01 19:03:19 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,60 @@
 class UsersController < ApplicationController
 
+  get "/signup" do
+      erb :signup
+    end
+
+    post "/signup" do
+      #your code here
+      user = User.new(:username => params[:username], :password => params[:password])
+      if user.username == ""
+        redirect "/failure"
+      end
+      if user.save
+  	     redirect "/login"
+  	  else
+  	     redirect "/failure"
+  	  end
+    end
+
+    get '/account' do
+      @user = User.find(session[:user_id])
+      erb :account
+    end
+
+
+    get "/login" do
+      erb :login
+    end
+
+    post "/login" do
+      ##your code here
+      user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/account"
+      else
+        redirect "/failure"
+      end
+    end
+
+    get "/failure" do
+      erb :failure
+    end
+
+    get "/logout" do
+      session.clear
+      redirect "/"
+    end
+
+    helpers do
+      def logged_in?
+        !!session[:user_id]
+      end
+
+      def current_user
+        User.find(session[:user_id])
+      end
+    end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ActiveRecord::Base
   has_secure_password
+  has_many :tweets
+
+  def slug
+    self.username.gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(sluggedstring)
+    @array1 = []
+    self.all.find do |inst|
+        inst.slug == sluggedstring
+    end
+  end
+
 end
